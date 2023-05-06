7f59fda083a73f4adc6bc26539f57791eba8fc38
Tarek Aziz <tarekiub17@gmail.com>
2023-05-06 23:51:51 +0600

    routing works!

--- side-navbar/src/app/app-routing.module.ts
+++ side-navbar/src/app/app-routing.module.ts
@@ -1,7 +1,24 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { ProductsComponent } from './products/products.component';
+import { MediaComponent } from './media/media.component';
+import { SettingsComponent } from './settings/settings.component';
+import { CoupensComponent } from './coupens/coupens.component';
+import { PagesComponent } from './pages/pages.component';
+import { StatisticsComponent } from './statistics/statistics.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  // this redirect will redirect to dashboard 
+  {path: '', redirectTo: 'dashboard', pathMatch: 'full'},
+  {path: 'dashboard', component: DashboardComponent},
+  {path: 'products', component: ProductsComponent},
+  {path: 'media', component: MediaComponent},
+  {path: 'settings', component: SettingsComponent},
+  {path: 'coupens', component: CoupensComponent},
+  {path: 'pages', component: PagesComponent},
+  {path: 'statistics', component: StatisticsComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
