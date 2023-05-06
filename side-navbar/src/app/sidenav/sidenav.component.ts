034600547cada756a33553b09e608dbd8508870f
Tarek Aziz <tarekiub17@gmail.com>
2023-05-07 01:15:34 +0600

    animation added

--- side-navbar/src/app/sidenav/sidenav.component.ts
+++ side-navbar/src/app/sidenav/sidenav.component.ts
@@ -1,5 +1,6 @@
-import { Component, Output, EventEmitter } from '@angular/core';
+import { Component, Output, EventEmitter, OnInit, HostListener } from '@angular/core';
 import { navbarData } from './navdata';
+import { animate, style, transition, trigger } from '@angular/animations';
 
 interface SideNavToggle{
   screenWidth: number;
@@ -9,13 +10,45 @@ interface SideNavToggle{
 @Component({
   selector: 'app-sidenav',
   templateUrl: './sidenav.component.html',
-  styleUrls: ['./sidenav.component.css']
+  styleUrls: ['./sidenav.component.css'],
+  animations: [
+    trigger('fadeInOut', [
+      transition(':enter', [
+        style({opacity:0}),
+        animate('50ms',
+          style({opacity:1})
+        )
+      ]),
+
+      transition(':leave', [
+        style({opacity:0}),
+        animate('50ms',
+          style({opacity:1})
+        )
+      ]),
+
+    ])
+  ]
 })
-export class SidenavComponent {
+export class SidenavComponent implements OnInit{
   @Output() onToggleSideNav: EventEmitter<SideNavToggle> = new EventEmitter();
   collapsed = false;
   screenWidth = 0;
   navData = navbarData;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event:any){
+    this.screenWidth = window.innerWidth;
+    if(this.screenWidth <= 768){
+      this.collapsed = false;
+      this.onToggleSideNav.emit({collapsed: this.collapsed, screenWidth: this.screenWidth});
+    }
+  }
+
+  ngOnInit(): void{
+    // console.log("clicked");
+    this.screenWidth = window.innerWidth;
+  }
 
   toggleClass(): void{
     this.collapsed = !this.collapsed;
