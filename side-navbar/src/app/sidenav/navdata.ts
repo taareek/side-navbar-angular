e8c893c0fd39d819f16e04de172e190f98ffcf53
Tarek Aziz <tarekiub17@gmail.com>
2023-05-06 20:24:09 +0600

    toggle button working

---
+++ side-navbar/src/app/sidenav/navdata.ts
@@ -0,0 +1,10 @@
+import { RouterLink } from "@angular/router";
+
+export const navbarData = [
+    {
+        routeLink: 'dashboard',
+        icon: 'fal fa-home',
+        label: 'Dashboard'
+    }
+
+];
